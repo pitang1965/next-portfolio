68a86c0ac999032a186cfbac36a22077b542dbfe
pitang1965 <pitang1965@gmail.com>
2022-08-30 19:41:48 +0900

    💄 Underline links if the related  page is shown

--- src/components/common/AnchorSelf.tsx
+++ src/components/common/AnchorSelf.tsx
@@ -1,5 +1,6 @@
 import React, { FC, ReactNode } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { Anchor } from '@mantine/core';
 
 type Props = {
@@ -9,12 +10,40 @@ type Props = {
 };
 
 export const AnchorSelf: FC<Props> = (props) => {
+  const router = useRouter();
+  console.log(`href: ${props.href} asPath: ${router.asPath}`);
+
+  // このリンク先に今いるかどうか
+  function isthePath(): boolean {
+    return props.href === router.asPath;
+  }
+
+  // このリンク先の下層ページに今いるかどうか
+  function isLowerPath(): boolean {
+    if (props.href === '/') {
+      return false;
+    }
+    return router.asPath.startsWith(props.href);
+  }
+
   return (
     <Link href={props.href}>
       <Anchor
         component='a'
         sx={(theme) => ({
-          color: props.color || theme.colorScheme === 'dark' ? theme.white : theme.black,
+          color:
+            props.color || theme.colorScheme === 'dark'
+              ? theme.white
+              : theme.black,
+          borderStyle: isthePath()
+            ? 'solid'
+            : isLowerPath()
+            ? 'dotted'
+            : 'none',
+          borderBottomWidth: '2px',
+          '&:hover': {
+            textDecoration: 'none',
+          },
         })}
       >
         {' '}
