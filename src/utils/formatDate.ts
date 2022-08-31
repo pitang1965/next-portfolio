70d88c65a0dc7f7511bcc1626c3569962c856af1
pitang1965 <pitang1965@gmail.com>
2022-08-31 12:18:26 +0900

    🎨 #31 use padStart

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,14 +1,14 @@
 export type DateFormat = 'YYYY.MM.DD' | 'YYYY.MM';
 
 // eslint-disable-next-line unused-imports/no-unused-vars
-const convToTwoDigitString = (value: number): string =>
-  10 < value ? String(value) : '0' + String(value);
+const convToTwoDigit = (value: number): string =>
+  String(value).padStart(2, '0');
 
 export const formatDate = (dateString: string, format: DateFormat) => {
   const date = new Date(dateString);
   return format === 'YYYY.MM.DD'
-    ? `${date.getFullYear()}.${convToTwoDigitString(
+    ? `${date.getFullYear()}.${convToTwoDigit(
         date.getMonth() + 1
-      )}.${convToTwoDigitString(date.getDate())}`
-    : `${date.getFullYear()}.${convToTwoDigitString(date.getMonth() + 1)}`;
+      )}.${convToTwoDigit(date.getDate())}`
+    : `${date.getFullYear()}.${convToTwoDigit(date.getMonth() + 1)}`;
 };
