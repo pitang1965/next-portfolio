fa05b167b9671fa3ab297962c718ab8d9d66f969
pitang1965 <pitang1965@gmail.com>
2022-09-07 19:43:21 +0900

    ✨ Add hyper links to Twitter user name and created date

--- src/components/twitter/TwitterCard.tsx
+++ src/components/twitter/TwitterCard.tsx
@@ -20,20 +20,21 @@ export const TwitterCard: FC<Props> = (props) => {
   return (
     <Card>
       <Group noWrap align='flex-start'>
-        <Avatar
-          radius='xl'
-          src={props.userImage}
-          alt={props.userScreenName}
-        />
+        <Avatar radius='xl' src={props.userImage} alt={props.userScreenName} />
         <Stack>
           <div className={styles.header}>
             <Text size='sm' weight={700}>
               {props.userScreenName}
             </Text>
-            <Text
-              size='xs'
-              color='dimmed'
-            >{`@${props.userName}・${props.data.created_at}`}</Text>
+            <a href={`https://twitter.com/${props.userName}`}>
+              <Text size='xs' color='dimmed'>{`@${props.userName}`}</Text>
+            </a>
+            <Text size='xs' color='dimmed'>
+              ・
+            </Text>
+            <a href={`https://twitter.com/${props.userName}/status/${props.data.id}`}>
+              <Text size='xs' color='dimmed'>{`${props.data.created_at}`}</Text>
+            </a>
           </div>
           <Text size='sm' weight={500}>
             {props.data.text}
