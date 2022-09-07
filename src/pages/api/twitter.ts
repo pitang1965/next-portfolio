3274e68fb2e6e7c47daa60859b4b97ae20c5aa99
pitang1965 <pitang1965@gmail.com>
2022-09-07 11:00:59 +0900

    ✨ Add /api/twitter

---
+++ src/pages/api/twitter.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Client } from 'twitter-api-sdk';
+
+export default async function getTweet(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    // Pass auth credentials to the library client
+    const twitterClient = new Client(process.env.TWITTER_BEARER_TOKEN as string);
+
+    const recentSearch = await twitterClient.tweets.tweetsRecentSearch({
+      //One query/rule/filter for matching Tweets. Refer to https://t.co/rulelength to identify the max query length
+      query: '(from:pitang1965)',
+
+      //A comma separated list of fields to expand.
+      expansions: ['author_id'],
+
+      //A comma separated list of User fields to display.
+      'tweet.fields': ['created_at'],
+
+      //The maximum number of results
+      max_results: 10,
+    });
+    console.log(recentSearch);
+
+    res.status(200).json(recentSearch);
+  } catch (err) {
+    console.log(err);
+    console.error(err);
+    res.statusCode = 500;
+  }
+}
