a17417309ca0b1b919e0871447e05a53273f8f0e
pitang1965 <pitang1965@gmail.com>
2022-09-09 22:37:04 +0900

    💄 Adust background color of footer in dark mode

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,15 @@ import { Center, Footer as MantineFooter, Text } from '@mantine/core';
 
 export const Footer = () => {
   return (
-    <MantineFooter height={60} p='md'>
+    <MantineFooter
+      height={60}
+      p='md'
+      sx={(theme) => ({
+        color: theme.colorScheme === 'dark' ? theme.black : theme.white,
+        backgroundColor:
+          theme.colorScheme === 'dark' ? theme.black : theme.white,
+      })}
+    >
       <Center>
         <Text
           variant='link'
