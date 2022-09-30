b0fe8bc7287b687c24ba6e8a1813a5f88854b6df
pitang1965 <pitang1965@gmail.com>
2022-09-30 17:29:57 +0900

    ✨ Codegen from GitHub Graph API

---
+++ codegen.ts
@@ -0,0 +1,22 @@
+import { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: [
+    {
+      'https://api.github.com/graphql': {
+        headers: {
+          authorization: `Bearer ${process.env.GITHUB_BEARER_TOKEN}`,
+        },
+      },
+    },
+  ],
+  documents: ['src/**/*.tsx'],
+  generates: {
+    'src/generated/graphql.ts': {
+      plugins: ['typescript'],
+    },
+  },
+};
+
+export default config;
